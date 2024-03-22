d5504d3c8cb7ff1009fe3e321e4e1783617a6f83
Lennon Araujo <lennon.araujo@corebiz.ag>
2024-03-22 15:11:15 -0300

    feat: config JWT/PASSPORT using algorithm RSA256

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,8 @@ export const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().optional().default(3333),
   DATABASE_URL: z.string().url(),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
